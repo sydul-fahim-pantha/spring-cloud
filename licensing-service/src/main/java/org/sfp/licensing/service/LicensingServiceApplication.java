76569ee36ad90377c114802838f3387fd4c2bb7a
Sydul Fahim Pantha <fahim.pantha@gmail.com>
2019-05-16 15:41:59 +0600

    First commit of a micro-service

---
+++ licensing-service/src/main/java/org/sfp/licensing/service/LicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sfp.licensing.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensingServiceApplication.class, args);
+	}
+
+}
